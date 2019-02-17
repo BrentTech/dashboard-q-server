b8f0852f34a74719b176a4c9d316babc270fc3ce
Brent Woodward <Brent.Woodward.1@gmail.com>
2019-02-16 16:01:10 -0800

    q server for dashboard lab

--- server.js
+++ server.js
@@ -3,15 +3,8 @@
 const Q = require('./lib/server.js');
 Q.start();
 
-
 const db = new Q('database');
-
 db.monitorEvents('create');
+db.monitorEvents('read');
 db.monitorEvents('update');
 db.monitorEvents('delete');
-
-
-const network = new Q('network');
-
-network.monitorEvents('attack');
-network.monitorEvents('no-service');
